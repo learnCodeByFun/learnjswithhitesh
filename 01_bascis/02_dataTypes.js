233545d716ae7c76d4f7ba795da60554765530e3
learnCodeByFun <156416385+learnCodeByFun@users.noreply.github.com>
2024-01-12 09:30:19 +0000

    data types

---
+++ 01_bascis/02_dataTypes.js
@@ -0,0 +1,12 @@
+// primitive data types
+// string = "", ''
+// number = 75475
+// bigInt = use for big numbers
+// boolean = true or false
+// symbol = unique findness
+// null = standalone value or empty value representaion
+// undefined = we do not provide or declare the value in variable
+
+// non-primitive data types
+// object
+// array
